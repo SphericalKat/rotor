3c39239c22930e38a966ed0640122eb21e1b3c56
SphericalKat <amolele@gmail.com>
2021-01-09 22:41:57 +0530

    feat: implement initial bot repl
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use teloxide::prelude::*;
+
+
+#[tokio::main]
+async fn main() {
+    run().await;
+}
+async fn run() {
+    dotenv().ok();
+    teloxide::enable_logging!();
+    log::info!("Starting bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |message| async move {
+        message.answer_dice().send().await?;
+        ResponseResult::<()>::Ok(())
+    }).await;
 }
