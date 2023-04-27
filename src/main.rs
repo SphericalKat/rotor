1fa124895066a3f0fac08bfc8f27237a077eca9f
SphericalKat <amolele@gmail.com>
2023-04-28 00:44:33 +0530

    feat: set up teloxide and tracing
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod commands;
+
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    // install global tracing collector, this is configured using the RUST_LOG env var
+    tracing_subscriber::fmt::init();
+
+    let bot = Bot::from_env();
 }
