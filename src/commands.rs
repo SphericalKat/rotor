51f0580d7b95e3ab2f2b3ac6ce7aae3d929d71cc
SphericalKat <amolele@gmail.com>
2023-04-28 00:59:50 +0530

    feat: set up basic command handling
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/commands.rs
+++ src/commands.rs
@@ -1,1 +1,30 @@
-enum
+use teloxide::utils::command;
+
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "The following commands are supported:"
+)]
+pub(crate) enum Command {
+    #[command(description = "display this text.")]
+    Help,
+
+    #[command(description = "Start the bot.")]
+    Start,
+}
+
+pub(crate) async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::Start => {
+            bot.send_message(msg.chat.id, "Welcome! I am a bot that takes code and executes it. To get started, please view the /help").await?
+        },
+    };
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 mod commands;
 
+use anyhow::Ok;
+use commands::{Command, answer};
 use teloxide::prelude::*;
+use tracing::info;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     // install global tracing collector, this is configured using the RUST_LOG env var
     tracing_subscriber::fmt::init();
+    info!("Initialized tracing");
+
+    // load env variables from .env file, but ignore error if it doesn't exist
+    dotenv::dotenv().ok();
+    info!("Loaded env variables");
 
     let bot = Bot::from_env();
+    info!("Bot is ready.");
+
+    Command::repl(bot, answer).await;
+
+    info!("Shutdown complete.");
+
+    Ok(())
 }
